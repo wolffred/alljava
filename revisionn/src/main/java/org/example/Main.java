105a1d308b3ad40e51e668502db654d8ce706481
Wilfred Abruquah <wilfred.abruquah@turntable.io>
2023-05-04 08:08:08 +0000

    revision

---
+++ revisionn/src/main/java/org/example/Main.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+//
+//        Map<String, Integer> studentID = new HashMap<>();
+
+//        studentID.put("Bob", 1);
+//        studentID.put("Bob", 2);
+//        studentID.put("Bib", 3);
+//
+//        System.out.println(studentID);
+//
+//        Map<String, Map<String, Integer>> classRooms = new HashMap<>();
+//
+//        classRooms.put("Class 1", studentID);
+//        studentID.put("Bab", 4);
+//
+//        System.out.println(classRooms);
+
+//        List<String> friends = new ArrayList<>();
+//        List<String> newFriends = new ArrayList<>();
+//
+//        Map<String, List> people = new HashMap<>();
+//
+//        friends.add("Tom");
+//        newFriends.add("Tom");
+//        friends.add("Tim");
+//        newFriends.add("Tim");
+//
+//        people.put("Thomas", friends);
+//        people.put("Matthew", newFriends);
+//        friends.add("Tam");
+//
+//        List<String > fri = people.get("Thomas");
+
+//        System.out.println(fri);
+//
+//        System.out.println(people);
+
+//        for (Map.Entry entry:people.entrySet()) {
+//            System.out.println(entry);
+//            System.out.println(entry.toString());
+//            System.out.println(entry.getValue());
+//            System.out.println(entry.getKey());
+//        }
+
+//        for (String key:people.keySet()) {
+//            System.out.println(Map.Entry.comparingByKey());
+//        }
+
+//        boolean go = people.containsValue(("Matthew"));
+//        if (people.containsKey("Matthew")){
+//
+//        }
+
+        //System.out.println(people.get("Matthew"));
+
+//        Map<String, Map<String, Integer>> companyDust = new HashMap<>();
+//
+//        Map<String, Integer> sonnidustbins = new HashMap<>();
+//        sonnidustbins.put("Upstairs", 5);
+//        sonnidustbins.put("Downstairs", 8);
+//
+//        Map<String, Integer> advdustbins = new HashMap<>();
+//        advdustbins.put("Upstairs", 5);
+//        advdustbins.put("Downstairs", 8);
+//
+//        companyDust.put("Son", sonnidustbins);
+//        companyDust.put("Advantage",advdustbins);
+//
+//        System.out.println(companyDust.get("Son").entrySet());
+//        System.out.println(companyDust.get("Son").values());
+
+
+
+
+
+    }
+}
